e94ad6e8b29e4b91629eba3e972265f6be716858
JDWTheNerdAutomaton <thenerdautomaton@gmail.com>
2021-05-25 13:30:36 +0900

    baseline app code to start the kata

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// initialize a basic express api with a single method to receive cloud events for the account upgrade
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+const router = express.Router();
+app.use("/", router);
+
+app.use(require("./routes/index"))
+
+
+app.listen(3000, () => {
+    'avaiting update events at http://localhost:3000/'
+});
+console.log("go")
